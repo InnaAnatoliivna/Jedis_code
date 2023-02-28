d93800109fa82ce346400064c9524394252da1bd
InnaAnatoliivna <120109442+InnaAnatoliivna@users.noreply.github.com>
2023-02-28 14:15:52 +0100

    +

--- src/modal.js
+++ src/modal.js
@@ -1,17 +1,19 @@
+// --------BOOK A TABLE
 (() => {
-    const refs = {
-      openModalBtn: document.querySelector("[data-modal-open]"),
-      closeModalBtn: document.querySelector("[data-modal-close]"),
-      modal: document.querySelector("[data-modal]"),
-    };
-  
-    refs.openModalBtn.addEventListener("click", toggleModal);
-    refs.closeModalBtn.addEventListener("click", toggleModal);
-  
-    function toggleModal() {
-      refs.modal.classList.toggle("is-hidden");
-    }
-  })();
+  const refs = {
+    openModalBtn: document.querySelectorA('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
+  //----------- BOOK A ROOM
   (() => {
     const refs = {
       openModalBtn: document.querySelector("[datar-modal-open]"),
@@ -25,7 +27,8 @@
     function toggleModal() {
       refs.modal.classList.toggle("is-hidden");
     }
-  })();
+})();
+  // PRICES PER ROOM
   (() => {
     const refs = {
       openModalBtn: document.querySelector("[datap-modal-open]"),
