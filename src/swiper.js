9eaba7977d4af6dfa8663273fdb5108fa2999945
Olexandr597 <Olexandr597@gmail.com>
2023-03-01 12:58:10 +0200

    update

--- src/swiper.js
+++ src/swiper.js
@@ -9,8 +9,6 @@ $('.swiper').slick({
   autoplay: true,
   autoplaySpeed: 2000,
   variableWidth: true,
-//   prevArrow: '<i class="arrow-left"></i>',
-//   nextArrow: '<i class="arrow-right"></i>',
   prevArrow: $(".arrow-left"),
   nextArrow: $(".arrow-right"),
   responsive: [
@@ -32,6 +30,38 @@ $('.swiper').slick({
     },
     {
       breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ],
+});
+
+$('.swiper-menu').slick({
+  dots: false,
+  infinite: true,
+  speed: 1000,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  centerMode: true,
+  centerPadding: '32px',
+  variableWidth: true,
+  prevArrow: $('.arrow-left'),
+  nextArrow: $('.arrow-right'),
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 375,
       settings: {
         slidesToShow: 1,
         slidesToScroll: 1,
