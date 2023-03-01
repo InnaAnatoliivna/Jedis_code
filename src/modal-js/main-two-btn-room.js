4f32428bc658cf0ab75999321fd088c9b8488c05
InnaAnatoliivna <120109442+InnaAnatoliivna@users.noreply.github.com>
2023-03-01 23:35:52 +0100

    + script for duplicate btn is the section hotel-main

--- src/modal-js/main-two-btn-room.js
+++ src/modal-js/main-two-btn-room.js
@@ -2,6 +2,7 @@
     const refs = {
         openModalBtn: document.querySelector('[data-modal-room-open-g]'),
         openNextModalBtn: document.querySelector('[data-modal-room-open-f]'),
+        openModalDuplicateBtn: document.querySelector('[data-modal-room-open-y]'),
         closeModalBtn: document.querySelector('[data-modal-room-close]'),
         modal: document.querySelector('[data-modal-room]'),
         body: document.querySelector('body'),
@@ -10,6 +11,7 @@
     refs.openModalBtn.addEventListener('click', toggleModal);
     refs.closeModalBtn.addEventListener('click', toggleModal);
     refs.openNextModalBtn.addEventListener('click', toggleModal);
+    refs.openModalDuplicateBtn.addEventListener('click', toggleModal);
 
     function toggleModal() {
         refs.modal.classList.toggle('is-hidden');
