a2eeeefc27516b53d02d05ed59ff85f6e7522e21
InnaAnatoliivna <120109442+InnaAnatoliivna@users.noreply.github.com>
2023-03-01 20:03:07 +0100

    +

--- src/main.js
+++ src/main.js
@@ -2,10 +2,10 @@ import './swiper';
 import './mobile-menu';
 import './hero-swiper';
 //--pages js
-import './modal-js/open-two-btn-room';
-import './modal-js/open-two-btn-table';
-import './modal-js/page-hotel-js/open-four-btn-prices';
-import './modal-js/page-hotel-js/open-one-btn-room';
+import './modal-js/main-two-btn-room';
+import './modal-js/main-two-btn-table';
+import './modal-js/page-hotel-js/hotel-four-btn-prices';
+import './modal-js/page-hotel-js/hotel-one-btn-room';
 import './modal-js/page-restaurant-js/open-two-btn-table';
 import './modal-js/page-room-js/open-one-btn-prices';
 import './modal-js/page-room-js/open-one-btn-room';
