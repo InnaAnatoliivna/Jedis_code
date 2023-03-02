a8bbd4c69b41349856d0d5c0f3c3d1849288823b
InnaAnatoliivna <120109442+InnaAnatoliivna@users.noreply.github.com>
2023-03-02 10:17:50 +0100

    +

--- src/swiper.js
+++ src/swiper.js
@@ -9,11 +9,11 @@ $('.swiper').slick({
   autoplay: true,
   autoplaySpeed: 2000,
   variableWidth: true,
-  prevArrow: $(".arrow-left"),
-  nextArrow: $(".arrow-right"),
+  prevArrow: $('.arrow-left'),
+  nextArrow: $('.arrow-right'),
   responsive: [
     {
-      breakpoint: 1024,
+      breakpoint: 1135,
       settings: {
         slidesToShow: 3,
         slidesToScroll: 3,
@@ -22,42 +22,10 @@ $('.swiper').slick({
       },
     },
     {
-      breakpoint: 600,
+      breakpoint: 768,
       settings: {
         slidesToShow: 2,
         slidesToScroll: 2,
-      },
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-      },
-    },
-    // You can unslick at a given breakpoint now by adding:
-    // settings: "unslick"
-    // instead of a settings object
-  ],
-});
-
-$('.swiper-menu').slick({
-  dots: false,
-  infinite: true,
-  speed: 1000,
-  slidesToShow: 4,
-  slidesToScroll: 1,
-  centerMode: true,
-  centerPadding: '32px',
-  variableWidth: true,
-  prevArrow: $('.arrow-left'),
-  nextArrow: $('.arrow-right'),
-  responsive: [
-    {
-      breakpoint: 768,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
       },
     },
     {
